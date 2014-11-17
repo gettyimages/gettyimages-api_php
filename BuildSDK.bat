d938d6bb7b69e219111eddcea3271b771882d727
CaseyMacPherson <casey@macphersonclan.org>
2014-11-17 08:03:06 -0800

    Start of adjustments to the batch file for windows building.

--- BuildSDK.bat
+++ BuildSDK.bat
@@ -1,18 +1,37 @@
 @echo off
 
 set outputDirectory=%~dp0%build\
+set currentDirectory=%~dp0%
+
+echo %currentDirectory%
+
+exit
 
 cls
 echo.
 echo Determining build directory structure
 echo.
 
-IF NOT EXIST %outputDirectory% mkdir %outputDirectory%
+IF NOT EXIST %outputDirectory%
+  echo Creating Directory %outputDirectory%
+  mkdir %outputDirectory%
 
 echo -------------------------------------------------------------
 echo Output Directory for Build: %outputDirectory%
 echo -------------------------------------------------------------
 echo.
+echo Checking Build Dependencies
+echo ..composer.phar
+
+IF NOT EXIST %currentDirectory%composer.phar (
+  echo Fetching composer.phar
+  IF EXIST %currentDirectory%composer.lock (del composer.lock)
+  curl -sS https://getcomposer.org/installer | php
+) else (
+  echo Updating composer.phar inplace
+  php composer.phar self-update
+)
+
 echo.
 echo --------------------Building PHP SDK-------------------------
 echo.
@@ -25,4 +44,20 @@ php buildPhar.php
 
 echo.
 echo.
-dir .\build
+echo Contents of Build output @: %outputDirectory%
+dir %outputDirectory%
+echo ;%PATH%; | find /C /I ";%currentDirectory%vendor/behat/behat/bin;"
+
+:: if [[ ":$PATH:" == *":${PWD}/vendor/behat/behat/bin:"* ]]; then
+::  echo Behat already in path skipping
+:: else
+::  echo Registering Behat in the path
+::  export PATH="${PATH}:${PWD}/vendor/behat/behat/bin"
+:: fi
+
+echo
+echo
+echo To run BDD scenarios
+echo php ./vendor/behat/behat/bin/behat
+echo or
+echo behat
